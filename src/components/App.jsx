3a7d03fb34f7f4d4f29a55db4db2874bcca2b85f
Serhii-Fesianov <littlefox06042000@gmail.com>
2024-01-11 02:10:04 +0200

    Finish logic and style.module this task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,56 @@
-import { FeedBack } from './FeedBack/FeedBack';
+import React, { Component } from 'react';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
+import { Statistics } from 'components/Statistics/Statistics';
 
-export const App = () => {
-  return <FeedBack />;
-};
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedback = option => {
+    this.setState(prevState => ({ [option]: prevState[option] + 1 }));
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    const { good } = this.state;
+    return Math.round((good * 100) / total);
+  };
+
+  render() {
+    const total = this.countTotalFeedback();
+    const positiveFeedback = this.countPositiveFeedbackPercentage();
+    const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            handleFeedback={this.handleFeedback}
+          />
+        </Section>
+        <Section>
+          <h2>Statistics</h2>
+          {total > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positiveFeedback={positiveFeedback}
+            />
+          ) : (
+            <p>No feedback</p>
+          )}
+        </Section>
+      </>
+    );
+  }
+}
